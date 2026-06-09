a52b401e853e52e1a1f8aaa715cf13fd4e3c3b84
mariodenny <dennym.channel@gmail.com>
2026-06-09 14:33:10 +0700

    Server js and bootstrap

--- server.js
+++ server.js
@@ -2,3 +2,35 @@
 // home.html, about.html , contact.html
 // optional pake boostrap
 // gunakan navbar untuk navigasi 3 file tersebut
+
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req,res) =>{
+    if(req.url === "/home" && req.method === "GET"){
+         fs.readFile('home.html', (err, data)=>{
+                    res.writeHead(200, {
+                        'Content-Type' : 'text/html'
+                    })
+        
+                    res.write(data)
+                    res.end()
+                })
+                return
+    }else{
+         fs.readFile('404.html', (err, data)=>{
+                    res.writeHead(404, {
+                        'Content-Type' : 'text/html'
+                    })
+        
+                    res.write(data)
+                    res.end()
+                })
+                return
+    }
+    res.end()
+})
+
+server.listen(5501, ()=>{
+    console.log("Server running on http://localhost:5501")
+})
